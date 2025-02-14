a7ab9c265aa5be8878c713c1ea2809ef13828253
MrPJ52 <myp0505@gmail.com>
2025-02-14 14:38:01 +0900

    Update practice_playwright.py
    Learn how to interact with website via playwright

--- practice_playwright.py
+++ practice_playwright.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import time
 
 p = sync_playwright().start()
 
@@ -6,6 +7,27 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://google.com")
+page.goto("https://www.wanted.co.kr/")
+time.sleep(5)
 
-page.screenshot(path="screenshot.png")
+page.click("button.Aside_searchButton__rajGo")
+time.sleep(5)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+time.sleep(5)
+
+page.keyboard.down("Enter")
+time.sleep(7)
+
+page.click("a#search_tab_position")
+time.sleep(5)
+
+
+for i in range(5):
+    page.keyboard.down("End")
+    time.sleep(3)
+
+
+print(page.content())
+
+p.stop()
