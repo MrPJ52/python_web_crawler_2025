40331b2276ef99f5a9348f31c5a16891196e6921
MrPJ52 <myp0505@gmail.com>
2025-02-17 14:48:43 +0900

    Update challenge_wanted.py
    Change way to get url from controlling the page to get query directily
    
    Error occured in case of finding python -> added try/except and counter for error

--- challenge_wanted.py
+++ challenge_wanted.py
@@ -21,25 +21,12 @@ class JobPageWanted:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
-        page.goto("https://www.wanted.co.kr/")
-        time.sleep(3)
-
-        page.click("button.Aside_searchButton__rajGo")
-        time.sleep(3)
-
-        page.get_by_placeholder("검색어를 입력해 주세요.").fill(self.keyword)
-        time.sleep(3)
-
-        page.keyboard.down("Enter")
-        time.sleep(5)
-
-        page.click("a#search_tab_position")
-        time.sleep(3)
-
+        page.goto("https://www.wanted.co.kr/search?query=" + self.keyword + "&tab=position")
+        time.sleep(2)
 
         for i in range(5):
             page.keyboard.down("End")
-            time.sleep(2)
+            time.sleep(1)
 
         self.soup = BeautifulSoup(page.content(), "html.parser")
 
@@ -47,12 +34,21 @@ class JobPageWanted:
 
     def find_jobs(self):
         jobs_raw_data = self.soup.find_all("div", role="listitem")
+        cnt = 0
+        err_cnt = 0
         for job in jobs_raw_data:
-            title = job.find("strong", class_="JobCard_title__HBpZf").text.strip()
-            company = job.find("span", class_="JobCard_companyName__N1YrF").text.strip()
-            reward = job.find("span", class_="JobCard_reward__cNlG5").text.strip()
-            url = "https://www.wanted.co.kr" + job.find("a")["href"]
-            self.jobs_list.append(Job(title, company, reward, url))
+            try:
+                title = job.find("strong", class_="JobCard_title__HBpZf").text.strip()
+                company = job.find("span", class_="JobCard_companyName__N1YrF").text.strip()
+                reward = job.find("span", class_="JobCard_reward__cNlG5").text.strip()
+                url = "https://www.wanted.co.kr" + job.find("a")["href"]
+                self.jobs_list.append(Job(title, company, reward, url))
+                cnt += 1
+            except:
+                err_cnt += 1
+                continue
+        
+        print(f"Scraping for {self.keyword} is done.\n Found {cnt} jobs, {err_cnt} errors occured.\n")
     
     def print_jobs(self):
         print(f"Jobs for {self.keyword}:\n")
@@ -67,7 +63,7 @@ class JobPageWanted:
     def export_csv(self):
         file = open("jobs_for_"+self.keyword+".csv", mode="w", encoding="utf-8", newline="")
         writter = csv.writer(file)
-        writter.writerow(["Title", "Company", "reward", "url"])
+        writter.writerow(self.jobs_list[0].__dict__.keys())
 
         for job_info in self.jobs_list:
             writter.writerow([job_info.title, job_info.company, job_info.reward, job_info.url])
@@ -75,5 +71,8 @@ class JobPageWanted:
     
 job_page_flutter = JobPageWanted("flutter")
 job_page_flutter.find_jobs()
-job_page_flutter.print_jobs()
 job_page_flutter.export_csv()
+
+job_page_python = JobPageWanted("python")
+job_page_python.find_jobs()
+job_page_python.export_csv()
