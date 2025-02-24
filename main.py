f2fff36693e75c8c5da995826abb825b4cb6870e
MrPJ52 <myp0505@gmail.com>
2025-02-24 23:31:05 +0900

    Update main.py
    Created pseudo-database

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from crawler_wanted import Job, CrawlerWanted
 
 app = Flask("JobScrapper")
 
+
+db = dict()
+
+
 @app.route("/")
 def home():
     return render_template("home.html", name = "Park")
@@ -14,9 +18,15 @@ def testing():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    crawler = CrawlerWanted(keyword)
-    crawler.find_jobs()
-    jobs_list = crawler.jobs_list
+
+    if keyword in db:
+        jobs_list = db[keyword]
+    else:
+        crawler = CrawlerWanted(keyword)
+        crawler.find_jobs()
+        jobs_list = crawler.jobs_list
+        db[keyword] = jobs_list
+    
     return render_template("search.html", keyword = keyword, jobs = jobs_list)
 
 
