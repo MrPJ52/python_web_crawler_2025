e7d67ca5613871d6d5d37230f0a692356f879f84
MrPJ52 <myp0505@gmail.com>
2025-02-24 16:09:53 +0900

    Practice flask rendering

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from crawler_wanted import Job, CrawlerWanted
 
 app = Flask("JobScraper")
 
 @app.route("/")
 def home():
-    return "Hello, world!"
+    return render_template("home.html", name = "Park")
 
+@app.route("/testing")
+def testing():
+    return "Testing..."
+
+#### if replit, run("0.0.0.0")
 app.run(debug=True)
